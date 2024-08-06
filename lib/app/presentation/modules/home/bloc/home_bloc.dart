1758712690a418260c43b1532de9d426190220f4
Luis <Luis.garcia.arostegui@gmail.com>
2024-08-07 00:55:35 +0200

    102. HomeAppBar

---
+++ lib/app/presentation/modules/home/bloc/home_bloc.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/foundation.dart';
+
+import 'home_state.dart';
+
+class HomeBloc extends ValueNotifier<HomeState>{
+  HomeBloc(super.value);
+  
+}
---
+++ lib/app/presentation/modules/home/bloc/home_state.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../../../domain/models/publication.dart';
+
+part 'home_state.freezed.dart';
+
+
+@freezed
+class HomeState with _$HomeState {
+  factory HomeState.loading() = _Loading;
+  factory HomeState.loaded({required List<Publication> publication}) = _Loaded;
+}
